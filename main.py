b25e39b33337a921e3268418365e8e5f6e1875a5
coba_comment <toyeex@gmail.com>
2020-08-26 05:52:04 +0700

    sequential

---
+++ main.py
@@ -0,0 +1,8 @@
+#Konstruksi Dasar Python
+#Sequential : Eksekusi perintah secara berurutan
+import datetime
+
+print("Halo Indonesia")
+print("By Ragowo")
+print("Tanggal :", datetime.datetime.now())
+print("-" * 39)
